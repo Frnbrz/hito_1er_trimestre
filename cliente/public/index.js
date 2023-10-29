ba4f34111db6b8e6154c423269d5c185b29b7362
frnbrz <frnbrz@gmail.com>
2023-10-29 21:14:05 +0100

    lint

--- cliente/public/index.js
+++ cliente/public/index.js
@@ -5,10 +5,10 @@ function login() {
   fetch('http://localhost:3000/api/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password }),
-  }).then(res => {
+    body: JSON.stringify({ email, password })
+  }).then((res) => {
     if (res.status === 200) {
-      res.json().then(data => {
+      res.json().then((data) => {
         localStorage.setItem('token', data.token)
         window.location.href = '/home.html'
       })
