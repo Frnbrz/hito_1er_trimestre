6f2b667fd9003a3d3bcc8bd4351dd8ab8394cc27
frnbrz <frnbrz@gmail.com>
2023-10-29 21:14:25 +0100

    typescript changes

--- servidor/src/types.d.ts
+++ servidor/src/types.d.ts
@@ -1,8 +1,8 @@
 export interface User {
-	id: number
-	name: string
-	email: string
-	password: string
+  id: number
+  name: string
+  email: string
+  password: string
 }
 
 export type newUserEntry = Omit<User, 'id'>
