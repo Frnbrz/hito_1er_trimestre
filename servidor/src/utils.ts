56377d844b4cd026f4bacdcd17b005d448ecd37d
frnbrz <frnbrz@gmail.com>
2023-10-30 08:09:45 +0100

    add status message

--- servidor/src/utils.ts
+++ servidor/src/utils.ts
@@ -50,3 +50,13 @@ function parsePassword(passwordFromRequest: any): string {
 
   return passwordFromRequest
 }
+
+export enum statusMessage {
+  OK = 'OK',
+  CREATED = 'CREATED',
+  BAD_REQUEST = 'BAD_REQUEST',
+  UNAUTHORIZED = 'UNAUTHORIZED',
+  NOT_FOUND = 'NOT_FOUND',
+  CONFLICT = 'CONFLICT',
+  INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR'
+}
