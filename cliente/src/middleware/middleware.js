3eaece31e7fab2953d595a913aa7a7e7cf7a3e0c
frnbrz <frnbrz@gmail.com>
2023-11-19 23:37:12 +0100

    Remove middleware.js file

--- cliente/src/middleware/middleware.js
+++
@@ -1,9 +0,0 @@
-export function middleware(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.authorization
-
-  if (token) {
-    next()
-  } else {
-    return res.redirect('index.html')
-  }
-}
