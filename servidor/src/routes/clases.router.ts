3e17e86853265c42b9933dfb785157d5913e2c0e
Frnbrz <franciscojose.brizamate.23@campusfp.es>
2023-11-24 11:32:50 +0100

    Add user and remove user functionality to clases router

--- servidor/src/routes/clases.router.ts
+++ servidor/src/routes/clases.router.ts
@@ -1,5 +1,10 @@
 import express from 'express'
-import { findClaseById, getClases } from '../services/clases.services'
+import {
+  addUserToClase,
+  findClaseById,
+  getClases,
+  removeUserFromClase
+} from '../services/clases.services'
 import { StatusMessage, StatusType } from '../types/enums'
 
 export const clasesRouter = express.Router()
@@ -11,6 +16,30 @@ clasesRouter.get('/', (_req, res) => {
 clasesRouter.get('/:id', (req, res) => {
   const id = req.params.id
   const clase = findClaseById(+id)
+
+  return clase !== undefined
+    ? res.send({ status: StatusType.OK, data: clase })
+    : res
+        .status(404)
+        .send({ status: StatusType.NOT_FOUND, message: StatusMessage.USER })
+})
+
+clasesRouter.post('/:id/users', (req, res) => {
+  const id = req.params.id
+  const userId = req.body.userId
+  const clase = addUserToClase(+id, userId)
+
+  return clase !== undefined
+    ? res.send({ status: StatusType.OK, data: clase })
+    : res
+        .status(404)
+        .send({ status: StatusType.NOT_FOUND, message: StatusMessage.USER })
+})
+
+clasesRouter.delete('/:id/users', (req, res) => {
+  const id = req.params.id
+  const userId = req.body.userId
+  const clase = removeUserFromClase(+id, userId)
 
   return clase !== undefined
     ? res.send({ status: StatusType.OK, data: clase })
--- servidor/src/services/clases.services.ts
+++ servidor/src/services/clases.services.ts
@@ -7,6 +7,28 @@ export function getClases(): Clase[] {
   return clases
 }
 
+export function addUserToClase(
+  claseId: number,
+  userId: number
+): Clase | undefined {
+  const clase = clases.find((clase) => clase.id === claseId)
+  if (clase !== undefined && clase.users.indexOf(userId) === -1) {
+    clase.users.push(userId)
+  }
+  return clase
+}
+
+export function removeUserFromClase(
+  claseId: number,
+  userId: number
+): Clase | undefined {
+  const clase = clases.find((clase) => clase.id === claseId)
+  if (clase !== undefined) {
+    clase.users = clase.users.filter((id) => id !== userId)
+  }
+  return clase
+}
+
 export function findClaseById(id: number): Clase | undefined {
   const clase = clases.find((clase) => clase.id === id)
   return clase
